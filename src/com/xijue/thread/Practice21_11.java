d0f5446d0ecce5da18e753d85a59d62f1a06f83a
Evan-dlut <evanyu.dlut@gmail.com>
2014-06-25 10:52:46 +0800

    增加了对于 ExecutorService 的使用，减少了创建线程的代码，通过循环模拟线程谢欢可能发生的实际情况

--- src/com/xijue/thread/Practice21_11.java
+++ src/com/xijue/thread/Practice21_11.java
@@ -2,26 +2,34 @@ package com.xijue.thread;
 
 import com.bitory.print.MyPrint;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by xijueyp on 14-6-24.
  */
 class Resouce{
-    private int value1;
-    private int value2;
+    private volatile int value1;
+    private volatile int value2;
+    private static int k = 0;
 
     public Resouce(){
         value1 = 1;
         value2 = 2;
     }
 
-    public synchronized void changeValues(){
+    public void changeValues() throws InterruptedException {
+        // synchronized 的多种用法？
+        // 联合成静态的
         value1+=value2;
-        Thread.yield();
+        int i=0;
+        while(i++<1000000);
         value2++;
         print();
     }
     public void print(){
-        MyPrint.printlnObject("value1:"+value1+","+"value2:"+value2);
+        MyPrint.printlnObject(k++ +"->value1:"+value1+","+"value2:"+value2);
     }
 
 }
@@ -33,21 +41,30 @@ public class Practice21_11 implements Runnable{
     }
 
     public static void main(String arv[]){
+        ExecutorService exec = Executors.newCachedThreadPool();
         Resouce r = new Resouce();
-        Practice21_11 p1 = new Practice21_11(r),
-                p2 = new Practice21_11(r),
-                p3 = new Practice21_11(r);
-
-        new Thread(p1).start();
-        new Thread(p2).start();
-        new Thread(p3).start();
-
-        //r.print();
+        exec.execute(new Practice21_11(r));
+        exec.execute(new Practice21_11(r));
+        exec.execute(new Practice21_11(r));
+        exec.execute(new Practice21_11(r));
+        exec.execute(new Practice21_11(r));
+        exec.execute(new Practice21_11(r));
+        exec.execute(new Practice21_11(r));
+        exec.execute(new Practice21_11(r));
+        exec.execute(new Practice21_11(r));
+        exec.execute(new Practice21_11(r));
+        exec.execute(new Practice21_11(r));
+        exec.execute(new Practice21_11(r));
+        exec.execute(new Practice21_11(r));
+        exec.execute(new Practice21_11(r));
     }
 
     @Override
     public void run() {
-        r.changeValues();
-
+        try {
+            r.changeValues();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
